5c0f876f84cdb85ceaa8ba4d941313295e0db5cd
Jonas0119 <jonaske@126.com>
2025-01-03 10:30:20 +0800

    address page is ok

--- pages/mine/addressForm.js
+++ pages/mine/addressForm.js
@@ -3,89 +3,59 @@ import { t } from '../../utils/i18n'
 
 Page({
   data: {
-    form: {
-      id: '',
-      receiveName: '',
-      receiveMobile: '',
-      address: ''
+    formData: {
+      defaultAddressFlag: 'Y'
     },
-    t: t
+    switched: true,
+    type: '',
+    id: '',
+    token: '',
+    locale: wx.getStorageSync('locale') || 'zh-Hant'
   },
 
-  onLoad: function(options) {
-    if (options.id) {
-      this.loadAddressDetail(options.id)
+  onLoad: function (option) {
+    this.setData({
+      type: option.type
+    })
+
+    let token = wx.getStorageSync('token')
+    if (token) {
+      this.setData({ token: token })
+      if (this.data.type == 'edit') {
+        this.setData({ id: option.id })
+        this.getInfo()
+      }
     }
   },
 
-  loadAddressDetail: function(id) {
+  onShow: function() {
+    // 在页面显示时检查语言是否变化
+    const currentLocale = wx.getStorageSync('locale') || 'zh-Hant'
+    if (currentLocale !== this.data.locale) {
+      this.setData({
+        locale: currentLocale
+      })
+    }
+  },
+
+  getInfo: function() {
     wx.request({
-      url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/detail',
+      url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/' + this.data.id,
       method: 'GET',
-      data: { id },
       header: {
-        'Authorization': `Bearer ${wx.getStorageSync('token')}`
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + this.data.token
       },
+      timeout: 6000,
+      sslVerify: false,
+      withCredentials: false,
+      firstIpv4: false,
       success: (res) => {
         if (res.data.code === 200) {
           this.setData({
-            form: res.data.data
+            formData: res.data.data,
+            switched: res.data.data.defaultAddressFlag == 'Y'
           })
-        }
-      }
-    })
-  },
-
-  onInput: function(e) {
-    const { field } = e.currentTarget.dataset
-    this.setData({
-      [`form.${field}`]: e.detail.value
-    })
-  },
-
-  saveAddress: function() {
-    const { form } = this.data
-
-    if (!form.receiveName) {
-      wx.showToast({
-        title: t('mine.inputName'),
-        icon: 'none'
-      })
-      return
-    }
-
-    if (!form.receiveMobile) {
-      wx.showToast({
-        title: t('mine.inputPhone'),
-        icon: 'none'
-      })
-      return
-    }
-
-    if (!form.address) {
-      wx.showToast({
-        title: t('mine.inputAddress'),
-        icon: 'none'
-      })
-      return
-    }
-
-    wx.request({
-      url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/save',
-      method: 'POST',
-      data: form,
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-        'Authorization': `Bearer ${wx.getStorageSync('token')}`
-      },
-      success: (res) => {
-        if (res.data.code === 200) {
-          wx.showToast({
-            title: t('common.success')
-          })
-          setTimeout(() => {
-            wx.navigateBack()
-          }, 1500)
         } else {
           wx.showToast({
             title: res.data.msg,
@@ -96,35 +66,104 @@ Page({
     })
   },
 
-  deleteAddress: function() {
-    wx.showModal({
-      title: t('common.tips'),
-      content: t('common.confirmDelete'),
+  onChange: function(e) {
+    this.setData({
+      switched: e.detail.value
+    })
+  },
+
+  onInput: function(e) {
+    const field = e.currentTarget.dataset.field
+    const value = e.detail.value
+    this.setData({
+      ['formData.' + field]: value
+    })
+  },
+
+  saveAddress: function() {
+    this.setData({
+      'formData.defaultAddressFlag': this.data.switched ? 'Y' : 'N'
+    })
+
+    if (!this.data.formData.receiveName) {
+      wx.showToast({
+        title: t('common.tipName'),
+        icon: 'none'
+      })
+      return
+    } else if (!this.data.formData.receiveMobile) {
+      wx.showToast({
+        title: t('common.tipPhone'),
+        icon: 'none'
+      })
+      return
+    } else if (!this.data.formData.address) {
+      wx.showToast({
+        title: t('common.tipAdd'),
+        icon: 'none'
+      })
+      return
+    }
+
+    const url = this.data.type == 'add' 
+      ? app.globalData.baseUrl + '/tjfae-space/goodsAddress/add'
+      : app.globalData.baseUrl + '/tjfae-space/goodsAddress/edit'
+    const method = this.data.type == 'add' ? 'POST' : 'PUT'
+
+    wx.request({
+      url: url,
+      method: method,
+      data: this.data.formData,
+      header: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + this.data.token
+      },
+      timeout: 6000,
+      sslVerify: false,
+      withCredentials: false,
+      firstIpv4: false,
       success: (res) => {
-        if (res.confirm) {
-          wx.request({
-            url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/delete',
-            method: 'POST',
-            data: { id: this.data.form.id },
-            header: {
-              'content-type': 'application/x-www-form-urlencoded',
-              'Authorization': `Bearer ${wx.getStorageSync('token')}`
-            },
-            success: (res) => {
-              if (res.data.code === 200) {
-                wx.showToast({
-                  title: t('common.success')
-                })
-                setTimeout(() => {
-                  wx.navigateBack()
-                }, 1500)
-              } else {
-                wx.showToast({
-                  title: res.data.msg,
-                  icon: 'none'
-                })
-              }
-            }
+        if (res.data.code === 200) {
+          wx.showToast({
+            title: t('common.success'),
+            icon: 'none'
+          })
+          setTimeout(() => {
+            wx.navigateBack({ delta: 1 })
+          }, 2000)
+        } else {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none'
+          })
+        }
+      }
+    })
+  },
+
+  onDel: function() {
+    wx.request({
+      url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/' + this.data.id,
+      method: 'DELETE',
+      header: {
+        'content-type': 'application/json',
+        'Authorization': 'Bearer ' + this.data.token
+      },
+      timeout: 6000,
+      sslVerify: false,
+      withCredentials: false,
+      firstIpv4: false,
+      success: (res) => {
+        if (res.data.code === 200) {
+          wx.showToast({
+            title: t('common.delsuccess'),
+            icon: 'none'
+          })
+          wx.navigateBack({ delta: 1 })
+        } else {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none'
           })
         }
       }
--- pages/mine/addressList.js
+++ pages/mine/addressList.js
@@ -3,103 +3,95 @@ import { t } from '../../utils/i18n'
 
 Page({
   data: {
-    addressList: [],
-    t: t
+    list: [],
+    token: '',
+    locale: wx.getStorageSync('locale') || 'zh-Hant'
   },
 
-  onLoad: function() {
-    this.loadAddressList()
-  },
-
-  onShow: function() {
-    this.loadAddressList()
-  },
-
-    loadAddressList: function () {
-        wx.request({
-            url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/list',
-            method: 'GET',
-            data: {
-                pageNum: 1,
-                pageSize: 100,
-            },
-            header: {
-                'content-type': "application/x-www-form-urlencoded",
-                'Authorization': `Bearer ${wx.getStorageSync('token')}`
-            },
-            timeout: 6000,
-            sslVerify: false,
-            withCredentials: false,
-            firstIpv4: false,
-            success: (res) => {
-                if (res.data.code === 200) {
-                    this.setData({
-                        addressList: res.data.rows
-                    })
-                }
-            }
-        })
-    },
-
-  selectAddress: function(e) {
-    const id = e.currentTarget.dataset.id
-    const pages = getCurrentPages()
-    const prevPage = pages[pages.length - 2]
-    
-    if (prevPage && prevPage.route.includes('pay')) {
-      const address = this.data.addressList.find(item => item.id === id)
-      prevPage.setData({
-        selectedAddress: address,
-        selectedAddressIndex: this.data.addressList.findIndex(item => item.id === id)
-      })
-      wx.navigateBack()
+  onLoad: function () {
+    let token = wx.getStorageSync('token')
+    if (token) {
+      this.setData({ token: token })
+      this.init()
     }
   },
 
-  addAddress: function() {
-    wx.navigateTo({
-      url: '/pages/mine/addressForm'
-    })
+  onShow: function () {
+    let token = wx.getStorageSync('token')
+    if (token) {
+      this.setData({ token: token })
+      this.init()
+    }
+    // 在页面显示时检查语言是否变化
+    const currentLocale = wx.getStorageSync('locale') || 'zh-Hant'
+    if (currentLocale !== this.data.locale) {
+      this.setData({
+        locale: currentLocale
+      })
+    }
   },
 
-  editAddress: function(e) {
-    const id = e.currentTarget.dataset.id
-    wx.navigateTo({
-      url: `/pages/mine/addressForm?id=${id}`
+  onBackPress: function() {
+    wx.switchTab({
+      url: '/pages/mine/index'
     })
+    return true
   },
 
-  deleteAddress: function(e) {
-    const id = e.currentTarget.dataset.id
-    wx.showModal({
-      title: t('common.tips'),
-      content: t('common.confirmDelete'),
+  init: function() {
+    wx.request({
+      url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/list',
+      method: 'GET',
+      data: {
+        pageNum: 1,
+        pageSize: 100
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Bearer ' + this.data.token
+      },
+      timeout: 6000,
+      sslVerify: false,
+      withCredentials: false,
+      firstIpv4: false,
       success: (res) => {
-        if (res.confirm) {
-          wx.request({
-            url: app.globalData.baseUrl + '/tjfae-space/goodsAddress/delete',
-            method: 'POST',
-            data: { id },
-            header: {
-              'content-type': 'application/x-www-form-urlencoded',
-              'Authorization': `Bearer ${wx.getStorageSync('token')}`
-            },
-            success: (res) => {
-              if (res.data.code === 200) {
-                wx.showToast({
-                  title: t('common.success')
-                })
-                this.loadAddressList()
-              } else {
-                wx.showToast({
-                  title: res.data.msg,
-                  icon: 'none'
-                })
-              }
-            }
+        if (res.data.code === 200) {
+          this.setData({
+            list: res.data.rows
+          })
+        } else if (res.data.code === '999999') {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none'
+          })
+          setTimeout(() => {
+            wx.removeStorage({ key: 'accountId' })
+            wx.removeStorage({ key: 'token' })
+            wx.removeStorage({ key: 'phone' })
+            wx.removeStorage({ key: 'headImg' })
+            wx.removeStorage({ key: 'name' })
+            wx.reLaunch({ url: '/pages/login/index' })
+          }, 2000)
+        } else {
+          wx.showToast({
+            title: res.data.msg,
+            icon: 'none'
           })
         }
       }
+    })
+  },
+
+  goDetailPage: function(e) {
+    const id = e.currentTarget.dataset.id
+    wx.navigateTo({
+      url: '/pages/mine/addressForm?type=edit&id=' + id
+    })
+  },
+
+  goNew: function() {
+    wx.navigateTo({
+      url: '/pages/mine/addressForm?type=add'
     })
   }
 }) 
